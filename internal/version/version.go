51712788a3d60c211c7410a138d5b7c8b0ee0537
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2023-09-25 18:44:27 +0200

    Best error message

--- internal/version/version.go
+++ internal/version/version.go
@@ -19,38 +19,36 @@ func fetchLatestRelease(owner, repo string) (*github.RepositoryRelease, error) {
 }
 
 func GetVersion(csvFile string) {
-
 	file, err := os.Open(csvFile)
+	if err != nil {
+		log.Fatal("Error while reading the file", err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Error reading records", err)
+	}
+
+	for _, item := range records[1:] {
+		cmd := exec.Command(item[0], "--version")
+		stdout, err := cmd.Output()
 		if err != nil {
-			log.Fatal("Error while reading the file", err)
+			log.Fatal("Error exec command", err)
 		}
 
-		defer file.Close()
+	latest, err := fetchLatestRelease(item[1], item[2])
+	if err != nil {
+		log.Fatal("Error fetch latest release", err)
+	}
 
-		reader := csv.NewReader(file)
-
-		records, err := reader.ReadAll()
-		if err != nil {
-			fmt.Println("Error reading records")
-		}
-
-		for _, item := range records[1:] {
-			cmd := exec.Command(item[0], "--version")
-			stdout, err := cmd.Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-		latest, err := fetchLatestRelease(item[1], item[2])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("Local version of %s %slast version in GitHub repo is %s\nurl %s\n\n",
-		    item[0],
-		    string(stdout),
-		    latest.GetTagName(),
-		    latest.GetHTMLURL())
-
-		}
+	fmt.Printf("Local version of %s %slast version in GitHub repo is %s\nurl %s\n\n",
+	    item[0],
+	    string(stdout),
+	    latest.GetTagName(),
+	    latest.GetHTMLURL())
+	}
 }
