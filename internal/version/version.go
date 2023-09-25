beb92142153cd3cd001dcde0a8fd6f207e5cabd9
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2023-09-25 14:53:14 +0200

    Formatted code

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,52 +1,56 @@
 package version
 
 import (
-    "context"
-	"encoding/csv"
+        "context"
+        "encoding/csv"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 
-    "github.com/google/go-github/v55/github"
+        "github.com/google/go-github/v55/github"
 )
 
 func fetchLatestRelease(owner, repo string) (*github.RepositoryRelease, error) {
-    client := github.NewClient(nil)
-    ctx := context.Background()
-    latest, _, err := client.Repositories.GetLatestRelease(ctx, owner, repo)
-    return latest, err
+	client := github.NewClient(nil)
+	ctx := context.Background()
+	latest, _, err := client.Repositories.GetLatestRelease(ctx, owner, repo)
+	return latest, err
 }
 
 func GetVersion(csvFile string) {
 
-    file, err := os.Open(csvFile)
-	if err != nil {
-		log.Fatal("Error while reading the file", err)
-	}
+	file, err := os.Open(csvFile)
+		if err != nil {
+			log.Fatal("Error while reading the file", err)
+		}
 
-	defer file.Close()
+		defer file.Close()
 
-	reader := csv.NewReader(file)
+		reader := csv.NewReader(file)
 
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error reading records")
-	}
+		records, err := reader.ReadAll()
+		if err != nil {
+			fmt.Println("Error reading records")
+		}
 
-	for _, item := range records[1:] {
-		cmd := exec.Command(item[0], "--version")
-		stdout, err := cmd.Output()
+		for _, item := range records[1:] {
+			cmd := exec.Command(item[0], "--version")
+			stdout, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		latest, err := fetchLatestRelease(item[1], item[2])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-        latest, err := fetchLatestRelease(item[1], item[2])
-        if err != nil {
-		    log.Fatal(err)
-		}
-       
-        fmt.Printf("Local version of %s %slast version in GitHub repo is %s\nurl %s\n\n", item[0], string(stdout), latest.GetTagName(), latest.GetHTMLURL())
+		fmt.Printf("Local version of %s %slast version in GitHub repo is %s\nurl %s\n\n",
+		    item[0],
+		    string(stdout),
+		    latest.GetTagName(),
+		    latest.GetHTMLURL())
 
-	}
+		}
 }
